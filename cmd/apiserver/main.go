a6ee865da5af0ac8682c07ca489f031ce0d69541
go-follow <gitshmelcer@gmail.com>
2020-03-22 14:37:19 +0700

    add main.go

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/go-follow/authorization.service/config"
+	"github.com/go-follow/authorization.service/pkg/logger"
+)
+
 func main() {
-	
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	fmt.Println(cfg)
 }
